576fa90c010f6780851ec1dc4b202444a29ffde9
baxaa <onalbekovbakbergen7@gmail.com>
2024-10-25 23:35:03 +0500

    diecee

---
+++ ios projects/Diecee/Dieceee/Dieceee/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dieceee
+//
+//  Created by Baqbergen Onalbekov on 13.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    let diceImages = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let randomIndex1 = Int.random(in: 0...5)
+        let randomIndex2 = Int.random(in: 0...5)
+        
+        // Устанавливаем изображения для каждого кубика
+        dice1.image = UIImage(named: diceImages[randomIndex1])
+        dice2.image = UIImage(named: diceImages[randomIndex2])
+    }
+    
+    
+}
+
